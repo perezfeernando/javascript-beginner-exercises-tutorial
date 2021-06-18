05b2215e61c1ddf66b275aa89543d5d79f576816
Fernando Pérez <fernandoperezdeveloper@gmail.com>
2021-06-18 19:28:55 +0000

    Updated JS Excercise Excuse Generator

--- exercises/05-User-inputed-variables/app.js
+++ exercises/05-User-inputed-variables/app.js
@@ -1,3 +1,5 @@
 var age = prompt('What is your age?');
 
 // Your code below:
+var numFinal = parseFloat(age);
+console.log(numFinal + 10);
--- exercises/09-Calling-your-first-function/app.js
+++ exercises/09-Calling-your-first-function/app.js
@@ -1,6 +1,9 @@
 function isOdd(myNumber)
 {
-	return !(myNumber % 2 == 0);
+    
+    return !(myNumber % 2 == 0);
 }
 
+
 // Your code below:
+console.log(isOdd(45345));
